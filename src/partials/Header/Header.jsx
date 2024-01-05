80beaf0961528bd5a0d0da3697ae5287aee38ced
Roman Telik <122562266+telezhka@users.noreply.github.com>
2024-01-05 13:22:46 +0200

    add-some-base-styles

--- src/partials/Header/Header.jsx
+++ src/partials/Header/Header.jsx
@@ -3,13 +3,17 @@ import logo from '../../images/icons/logo.svg';
 export const Header = () => {
   return (
     <header id="top">
-      <div>
+      <div className="container header-container">
         <a href="#top">
           <img src={logo} className="logo" alt="logo" />
         </a>
         <nav>
-          <a href="#users">Users</a>
-          <a href="#signup">Sign up</a>
+          <a href="#users" className="button">
+            Users
+          </a>
+          <a href="#signup" className="button">
+            Sign up
+          </a>
         </nav>
       </div>
     </header>
