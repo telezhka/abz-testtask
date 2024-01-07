f20256e1beb0cab2a9f66b5860d22255eaec0e30
Roman Telik <rtelik58@gmail.com>
2024-01-07 23:07:22 +0200

    test-regex-userpic

--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -1,14 +1,17 @@
+import defaultPic from '../../images/icons/photo-cover.svg';
+
 export const UserList = ({ users }) => {
   if (!Array.isArray(users)) {
     return <p>No users available</p>;
   }
-
+  const regex =
+    /https:\/\/frontend-test-assignment-api\.abz\.agency\/images\/users\/([0-9]+([A-Za-z]+[0-9]+)+)\.[A-Za-z]+/i;
   return (
     <ul className="users-list">
       {users.map(user => (
         <li key={user.id} className="user-item">
           <img
-            src={user.photo}
+            src={regex.test(user.photo) ? user.photo : defaultPic}
             alt={user.name}
             className="user-pic"
             title={user.name}
