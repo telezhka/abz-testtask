56d2906c37fbce23eee05a3b7230cddb822429cd
Roman Telik <rtelik58@gmail.com>
2024-01-09 01:01:03 +0200

    cleared-code

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,15 +1,17 @@
 export const getContacts = state => state.contacts;
 
-export const getToken = state => state.token.token;
-
-export const getPositions = state => state.positions.positions;
+export const getUsersError = state => state.contacts.error;
 
 export const getSentSuccess = state => state.contacts.send_result;
 
+export const getIsLoadingContacts = state => state.contacts.isLoading;
+
+export const getToken = state => state.token.token;
+
 export const getError = state => state.token.error;
 
-export const getUsersError = state => state.contacts.error;
-
 export const getIsLoadingToken = state => state.token.isLoading;
-export const getIsLoadingContacts = state => state.contacts.isLoading;
+
 export const getIsLoadingPositions = state => state.positions.isLoading;
+
+export const getPositions = state => state.positions.positions;
